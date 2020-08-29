dd952e6bf205d07cce081eae9000de74bafb888e
Yerson Arvelo <yerson.a06@gmail.com>
2020-08-28 23:11:51 -0400

    added function to active timer on start button click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,109 +1,35 @@
-// variables to keep track of quiz state
-var currentQuestionIndex = 0;
-var time = questions.length * 15;
-var timerId;
+// variable to keep track of score
+var score = 0;
+
+var questionIndex = 0;
 
 // variables to reference DOM elements
-var questionsEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
-var choicesEl = document.getElementById("choices");
-var submitBtn = document.getElementById("submit");
-var startBtn = document.getElementById("start");
-var initialsEl = document.getElementById("initials");
-var feedbackEl = document.getElementById("feedback");
-
-// sound effects
-var sfxRight = new Audio("assets/sfx/correct.wav");
-var sfxWrong = new Audio("assets/sfx/incorrect.wav");
-
-function startQuiz() {
-  // hide start screen
- 
-  // un-hide questions section
-
-  // start timer
-
-  // show starting time
-
-  getQuestion();
-}
-
-function getQuestion() {
-  // get current question object from array
-
-  // update title with current question
-
-  // clear out any old question choices
-
-  // loop over choices
-
-    // create new button for each choice
-
-    // attach click event listener to each choice
-
-    // display on the page
-}
-
-function questionClick() {
-  // check if user guessed wrong
-    // penalize time
-
-    // display new time on page
-
-    // play "wrong" sound effect
-
-  // else 
-    // play "right" sound effect
+var questionsEl = document.getElementById("questions"); //CHECK
+var timerEl = document.getElementById("time"); // CHECK
+var startBtn = document.getElementById("start");  // CHECK
+var wrapper = document.querySelector("#wrapper"); // CHECK
 
 
-  // flash right/wrong feedback on page for half a second
+var timeRemaining = 75;
+var timeInterval = 0;
+var timePenalty = 5;
+var questionsList = document.createElement("ul");
 
-  // move to next question
+// on start button click, trigger timer and display first question
+startBtn.addEventListener("click", function () {
+  if (timeInterval === 0) {
+    timeInterval = setInterval(function () {
+      timeRemaining--;
+      timerEl.textContent = "Time Remaining: " + timeRemaining;
 
-  // check if we've run out of questions
-    // quizEnd
-  // else 
-    // getQuestion
-}
+      if (timeRemaining <= 0) {
+        clearInterval(timeInterval);
+        quizEnd();
+        timerEl.textContent = "Time's up!";
+      }
+    }, 1000);
+  }
+  displayQuestions(questionIndex);
+});
 
-function quizEnd() {
-  // stop timer
 
-  // show end screen
-
-  // show final score
-
-  // hide questions section
-}
-
-function clockTick() {
-  // update time
-
-  // check if user ran out of time
-}
-
-function saveHighscore() {
-  // get value of input box
-
-  // make sure value wasn't empty
-    // get saved scores from localstorage, or if not any, set to empty array
-
-    // format new score object for current user
-
-    // save to localstorage
-
-    // redirect to next page
-}
-
-function checkForEnter(event) {
-  // check if event key is enter
-    // saveHighscore
-}
-
-// user clicks button to submit initials
-submitBtn.onclick = saveHighscore;
-
-// user clicks button to start quiz
-startBtn.onclick = startQuiz;
-
-initialsEl.onkeyup = checkForEnter;
